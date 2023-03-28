e321ab821428f083dd70dfbef9e5e14030840cb0
LunarStrain94 <nikolabos00@gmail.com>
2023-03-28 05:51:33 +0200

    Added most of the models

--- src/DSMS.Application/Models/User/CreateUserModel.cs
+++ src/DSMS.Application/Models/User/CreateUserModel.cs
@@ -2,11 +2,27 @@
 
 public class CreateUserModel
 {
+    public Guid Id { get; set; }
+
+    public Guid RoleId { get; set; }
+
     public string Username { get; set; }
 
     public string Email { get; set; }
 
     public string Password { get; set; }
+
+    private string FirstName { get; set; }
+
+    private string LastName { get; set; }
+
+    private DateTime DateOfBirth { get; set; }
+
+    private byte[] ProfilePicture { get; set; }
+
+    private string Oib { get; set; }
+
+    private string IdCardNumber { get; set; }
 }
 
 public class CreateUserResponseModel : BaseResponseModel { }
