4659555e4ab622ac85e8db55d562e6e90fd0f57c
Anamarija Papić <ap48790@oss.unist.hr>
2023-04-23 22:33:19 +0200

    Add default authentication features
    - Convert project to Authentication type: Individual Accounts instead of None
    - Scaffold Identity in DSMS.Frontend project
    - Publish default Identity pages
    - Login/logout & register are working (but should be adjusted later)

--- src/DSMS.Frontend/Program.cs
+++ src/DSMS.Frontend/Program.cs
@@ -1,12 +1,31 @@
+﻿using DSMS.Core.Entities.Identity;
+using DSMS.DataAccess.Persistence;
+using DSMS.Shared.Services;
+using DSMS.Shared.Services.Impl;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<DatabaseContext>(options =>
+    options.UseNpgsql(connectionString));
+builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+
+builder.Services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true)
+    .AddEntityFrameworkStores<DatabaseContext>();
 builder.Services.AddRazorPages();
+
+builder.Services.AddScoped<IClaimService, ClaimService>();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
+if (app.Environment.IsDevelopment())
+{
+    app.UseMigrationsEndPoint();
+}
+else
 {
     app.UseExceptionHandler("/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
@@ -18,6 +37,7 @@ app.UseStaticFiles();
 
 app.UseRouting();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapRazorPages();
