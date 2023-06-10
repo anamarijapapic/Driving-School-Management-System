46c6aeab7d403d7df18088b2d86e90763b0e22f1
Anamarija Papić <ap48790@oss.unist.hr>
2023-06-10 23:18:41 +0200

    Users - Delete

---
+++ src/DSMS.Frontend/Pages/Users/Delete.cshtml.cs
@@ -0,0 +1,55 @@
+﻿#nullable disable
+
+using DSMS.Core.Entities.Identity;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace DSMS.Frontend.Pages.Users
+{
+    [Authorize(Roles = ("Administrator"))]
+    public class DeleteModel : PageModel
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public DeleteModel(
+            UserManager<ApplicationUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        public string Username { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(string Id)
+        {
+            var user = await _userManager.FindByIdAsync(Id);
+            if (user == null)
+            {
+                return base.NotFound($"Unable to load user with ID '{Id}'.");
+            }
+
+            var userName = await _userManager.GetUserNameAsync(user);
+            Username = userName;
+
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync(string Id)
+        {
+            var user = await _userManager.FindByIdAsync(Id);
+            if (user == null)
+            {
+                return NotFound($"Unable to load user with ID '{Id}'.");
+            }
+
+            var result = await _userManager.DeleteAsync(user);
+            if (!result.Succeeded)
+            {
+                throw new InvalidOperationException($"Unexpected error occurred deleting user.");
+            }
+
+            return Redirect("~/Users/Index");
+        }
+    }
+}
