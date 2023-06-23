40baee29417bc3aa3c4f8ee3a5d9c477473d3b75
Nikola Occidentale <nikolaoccid@gmail.com>
2023-06-23 08:38:24 +0200

    Draft add feedback frontend

--- src/DSMS.Application/MappingProfiles/FeedbackProfile.cs
+++ src/DSMS.Application/MappingProfiles/FeedbackProfile.cs
@@ -15,4 +15,4 @@ namespace DSMS.Application.MappingProfiles
         }
         }
     }
-}
+
--- src/DSMS.Frontend/Pages/Users/Details.cshtml.cs
+++ src/DSMS.Frontend/Pages/Users/Details.cshtml.cs
@@ -1,27 +1,42 @@
 ﻿#nullable disable
 
+using AutoMapper;
+using DSMS.Application.Models.Feedback;
+using DSMS.Application.Services;
 using DSMS.Core.Entities.Identity;
 using DSMS.DataAccess.Repositories;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Identity;
+using System.Security.Claims;
 
 namespace DSMS.Frontend.Pages.Users
 {
     public class DetailsModel : PageModel
     {
+        private readonly Mapper _mapper;
         private readonly IUserRepository _userRepository;
         private readonly UserManager<ApplicationUser> _userManager;
+        private readonly IFeedbackService _feedbackService;
 
         public DetailsModel(IUserRepository userRepository,
-            UserManager<ApplicationUser> userManager)
+            UserManager<ApplicationUser> userManager,
+            IFeedbackService feedbackService,
+            Mapper mapper)
         {
             _userRepository = userRepository;
             _userManager = userManager;
+            _feedbackService = feedbackService;
+            _mapper = mapper;
         }
+
+        [BindProperty]
+        public CreateFeedbackModel Input { get; set; }
 
         public ApplicationUser ApplicationUser { get; private set; }
         public string UserRole { get; private set; }
+
 
         public async Task<IActionResult> OnGetAsync(string Id)
         {
@@ -38,6 +53,26 @@ namespace DSMS.Frontend.Pages.Users
             UserRole = roles.First();
 
             return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid) return Page();
+            
+            var feedback = _mapper.Map<CreateFeedbackModel>(Input);
+            var student = _userManager.FindByIdAsync(User.FindFirstValue(ClaimTypes.NameIdentifier));
+            feedback.InstructorId = ApplicationUser.Id;
+
+
+            try
+            {
+                await _feedbackService.CreateAsync(feedback);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            return Redirect("~/Users/Index");
         }
     }
 }
