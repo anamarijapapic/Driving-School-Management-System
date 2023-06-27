10bb795ead52691af4c649e0d6ca114a28d3ea4c
Nikola Occidentale <nikolaoccid@gmail.com>
2023-06-27 14:49:16 +0200

    Draft AppointmentRepository to use IQueryable

--- src/DSMS.DataAccess/Repositories/Impl/AppointmentRepository.cs
+++ src/DSMS.DataAccess/Repositories/Impl/AppointmentRepository.cs
@@ -10,39 +10,54 @@ namespace DSMS.DataAccess.Repositories.Impl
     {
         public AppointmentRepository(DatabaseContext context) : base(context) { }
 
-        public async Task<IEnumerable<Appointment>> GetAllAsync()
+        private IQueryable<Appointment> GetAll()
         {
-            return await DbSet
-                .Include(a => a.Instructor)
-                .Include(a => a.Student)
-                .OrderBy(a => a.Status)
-                    .ThenBy(a => a.Date)
-                        .ThenBy(a => a.Start)
-                .ToListAsync();
+            return DbSet
+             .Include(a => a.Instructor)
+             .Include(a => a.Student)
+             .OrderBy(a => a.Status)
+                 .ThenBy(a => a.Date)
+                     .ThenBy(a => a.Start)
+             .AsQueryable();
         }
 
-        public async Task<IEnumerable<Appointment>> GetByInstructorAsync(ApplicationUser instructor)
+        public async Task<IEnumerable<Appointment>> GetAllAsync()
         {
-            return await DbSet
+            return await GetAll().ToListAsync();
+        }
+
+        private IQueryable<Appointment> GetByInstructor(ApplicationUser instructor)
+        {
+            return DbSet
                 .Include(a => a.Instructor)
                 .Include(a => a.Student)
                 .Where(a => a.Instructor == instructor)
                 .OrderBy(a => a.Status)
                     .ThenBy(a => a.Date)
                         .ThenBy(a => a.Start)
-                .ToListAsync();
+                .AsQueryable();
         }
 
-        public async Task<IEnumerable<Appointment>> GetByStudentAsync(ApplicationUser student)
+        public async Task<IEnumerable<Appointment>> GetByInstructorAsync(ApplicationUser instructor)
         {
-            return await DbSet
+            return await GetByInstructor(instructor).ToListAsync();
+        }
+
+        private IQueryable<Appointment> GetByStudent(ApplicationUser student)
+        {
+            return DbSet
                 .Include(a => a.Instructor)
                 .Include(a => a.Student)
                 .Where(a => a.Student == student)
                 .OrderBy(a => a.Status)
                     .ThenBy(a => a.Date)
                         .ThenBy(a => a.Start)
-                .ToListAsync();
+                .AsQueryable();
+        }
+
+        public async Task<IEnumerable<Appointment>> GetByStudentAsync(ApplicationUser student)
+        {
+            return await GetByStudent(student).ToListAsync();
         }
 
         public Appointment GetById(string id)
@@ -54,10 +69,10 @@ namespace DSMS.DataAccess.Repositories.Impl
                 .FirstOrDefault();
         }
 
-        public async Task<IEnumerable<TimeOnly>> GetReservedSlotsByInstructorAndDateAsync(ApplicationUser instructor,
+        private IQueryable<TimeOnly> GetReservedSlotsByInstructorAndDate(ApplicationUser instructor,
             DateOnly date)
         {
-            return await DbSet
+            return DbSet
                 .Include(a => a.Instructor)
                 .Include(a => a.Student)
                 .Where(a => a.Instructor == instructor)
@@ -66,7 +81,14 @@ namespace DSMS.DataAccess.Repositories.Impl
                 .OrderBy(a => a.Date)
                     .ThenBy(a => a.Start)
                 .Select(a => a.Start)
-                .ToListAsync();
+                .AsQueryable();
+        }
+
+        public async Task<IEnumerable<TimeOnly>> GetReservedSlotsByInstructorAndDateAsync(ApplicationUser instructor,
+            DateOnly date)
+        {
+            
+               return await GetReservedSlotsByInstructorAndDate(instructor, date).ToListAsync();
         }
     }
 }
